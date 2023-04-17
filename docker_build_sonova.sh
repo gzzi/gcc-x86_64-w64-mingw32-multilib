bd05c31dc153361775a4cf95c8d4dddceadb8d76
Guillaume Galeazzi <24816508+gzzi@users.noreply.github.com>
2023-04-17 07:35:48 +0200

    helios target only

--- docker_build_sonova.sh
+++ docker_build_sonova.sh
@@ -5,7 +5,8 @@ source docker_build_helper.sh
 prepare_container
 
 BUILD="$(gcc -dumpmachine)"
-for TARGET in "x86_64-w64-mingw32" "arc-elf" "arm-none-eabi" "$BUILD"; do
+# for TARGET in "x86_64-w64-mingw32" "arc-elf" "arm-none-eabi" "$BUILD"; do
+for TARGET in "x86_64-w64-mingw32" "arm-none-eabi"; do
 	for HOST in "$(gcc -dumpmachine)" "x86_64-w64-mingw32"; do
 		if [ "$TARGET" == "$BUILD" ] && [ "$HOST" != "$TARGET" ]; then
 			echo "Skipping $HOST -> $TARGET"
